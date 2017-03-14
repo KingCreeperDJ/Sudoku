48628cde414ba3754019ab2712d69d60bfcf0354
KingCreeperDJ <123kingcreeper@gmail.com>
2017-03-14 22:21:41 +0100

    Java code insert
    My preciously made java code imported into the

--- src/main/java/Sudoku/Main.java
+++ src/main/java/Sudoku/Main.java
@@ -11,11 +11,22 @@ public final class Main {
     /** Launches the browser */
     public static void main(String... args) throws Exception {
         BrowserBuilder.newBrowser().
-            loadPage("pages/index.html").
+            loadPage("Sudoku/index.html").
             loadClass(Main.class).
             invoke("onPageLoad", args).
             showAndWait();
         System.exit(0);
+        
+        Field test = new Field();
+        test.createField(9, 9);
+        test.insert(1, 1, 5, test);
+        
+        test.fillField(test);
+        test.printArray(test);
+        
+        Field test2 = new Field();
+        test2.createField(3, 3);
+        test.printArray(test2);
     }
     
     /** Called when page is ready */
